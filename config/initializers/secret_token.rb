06ade82b29e15cb8de80b3355b3b271d0e1e99b2
root <root@ip-10-228-174-15.eu-west-1.compute.internal>
2012-03-03 20:58:24 +0000

    Initial rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Www::Application.config.secret_token = '34b4e24df1dd4ae34b476b74d888649756513bec293410672b70bd5dd26eecb0281aafc929e06bc90c7c955c632e02a529058951d6a09348587a97bb451829c4'
