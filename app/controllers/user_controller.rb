8b127bcc1319f5abb2b92221620eb38b649164e3
pniziniecki@gmail.com <pniziniecki@gmail.com>
2012-04-10 01:45:00 +0200

    little fix

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,6 +7,9 @@ class UserController < ApplicationController
 			@user = User.new params[:signup][:user]
 			@company = Company.new params[:signup][:company]
 			
+			@user.valid?
+			@company.valid?
+			
 			if @user.valid? && @company.valid?
 				
 				#TODO: send mail
