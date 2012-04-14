d8f1a5744ea32c5298daebbda67333ca71544889
Pawel Batko <pawel.batko@gmail.com>
2012-04-14 15:15:03 +0200

    Authorize users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
 
   def index
   end
@@ -36,11 +38,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find params[:id]
   end
 
   def update
-    @user = User.find params[:id]
     if @user.update_attributes params[:user]
       flash[:success] = "Profile updated"
       sign_in @user
@@ -54,4 +54,15 @@ class UsersController < ApplicationController
 	def destroy
   end
 
+private
+  
+  def signed_in_user
+    store_location
+    redirect_to signin_path, notice: "Please sign in"  unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find params[:id]
+    redirect_to root_path unless current_user? @user
+  end
 end
