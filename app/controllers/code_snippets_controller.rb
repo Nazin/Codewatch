ad40eddfbe3ab9e925ff3b047dfd5bded451ca78
Pawel Batko <pawel.batko@gmail.com>
2012-04-14 22:58:18 +0200

    hmm

--- app/controllers/code_snippets_controller.rb
+++ app/controllers/code_snippets_controller.rb
@@ -1,53 +1,53 @@
 class CodeSnippetsController < ApplicationController
-  include CodeSnippetsHelper
+	include CodeSnippetsHelper
 
-  def index
-    @code_snippets = CodeSnippet.all
-  end
-  
-  def new
-    @lexers = @@lexers
-    @code_snippet = CodeSnippet.new
-  end
+	def index
+		@code_snippets = CodeSnippet.all
+	end
+	
+	def new
+		@lexers = @@lexers
+		@code_snippet = CodeSnippet.new
+	end
 
-  def create
-    @code_snippet = CodeSnippet.new params[:code_snippet]
-    @code_snippet.sha = sha1 @code_snippet.code
-    if @code_snippet.save
-      redirect_to code_snippet_sha_path(@code_snippet)
-    else
-      @lexers = @@lexers
-      flash.now[:warning] =  "Invalid form input"
-      render 'new'
-    end
-  end
+	def create
+		@code_snippet = CodeSnippet.new params[:code_snippet]
+		@code_snippet.sha = sha1 @code_snippet.code
+		if @code_snippet.save
+			redirect_to code_snippet_sha_path(@code_snippet)
+		else
+			@lexers = @@lexers
+			flash.now[:warning] =  "Invalid form input"
+			render 'new'
+		end
+	end
 
-  def show
-    @code_snippet = CodeSnippet.find_by_sha params[:sha]
-    unless @code_snippet
-      redirect_to code_snippets_path, flash: {error: "Code snippet not found"}
-    else
-      @lines = @code_snippet.code.lines.count
-      lexer = @code_snippet.lang
-      if lexer
-        @highlighted =  Pygments.highlight(@code_snippet.code, :lexer => lexer )
-      else
-        @highlighted = @code_snippet.code
-      end
-    end
-  end
+	def show
+		@code_snippet = CodeSnippet.find_by_sha params[:sha]
+		unless @code_snippet
+			redirect_to code_snippets_path, flash: {error: "Code snippet not found"}
+		else
+			@lines = @code_snippet.code.lines.count
+			lexer = @code_snippet.lang
+			if lexer
+				@highlighted =  Pygments.highlight(@code_snippet.code, :lexer => lexer )
+			else
+				@highlighted = @code_snippet.code
+			end
+		end
+	end
 
 
 
-private
+	private
 
-  class << self
-    @@lexers =  Pygments::Lexer.all.sort { |a,b| a.name.downcase <=> b.name.downcase }
-    @@lexers =  @@lexers.collect { |l| [l.name, l.aliases.first] }
-  end
+	class << self
+		@@lexers =  Pygments::Lexer.all.sort { |a,b| a.name.downcase <=> b.name.downcase }
+		@@lexers =  @@lexers.collect { |l| [l.name, l.aliases.first] }
+	end
 
 	def sha1 code
-    salt = random_string 5
+		salt = random_string 5
 		Digest::SHA1.hexdigest(salt + code + salt)
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
-  before_filter :admin_user,     only: :destroy
+  before_filter :admin_user,		 only: :destroy
   before_filter :registered_user, only: [:new, :create]
   def index
     @users = User.paginate page: params[:page]
@@ -11,9 +11,9 @@ class UsersController < ApplicationController
     @user = User.new
     user_companies = @user.user_companies.build
     user_companies.build_company
- 
+    
   end
-
+  
   def create
     @user = User.new params[:user]
     if @user.save
@@ -21,11 +21,11 @@ class UsersController < ApplicationController
       sign_in @user
       redirect_to @user
     else
-      flash.now[:warning] =  "Invalid informations"
+      flash.now[:warning] =	 "Invalid informations"
       render 'new'
     end
-  end
-
+	end
+  
   def show
     @user = User.find params[:id]
     @user_companies = @user.user_companies
@@ -39,47 +39,47 @@ class UsersController < ApplicationController
     #logger.warn warning
     redirect_to root_path
   end
-
+  
   def edit
   end
-
+  
   def update
-    if @user.update_attributes params[:user]
-      flash[:success] = "Profile updated"
-      sign_in @user
-      redirect_to @user
-    else
-      flash.now[:warning] =  "Invalid informations"
-      render 'edit'
-    end
-  end
+          if @user.update_attributes params[:user]
+            flash[:success] = "Profile updated"
+            sign_in @user
+			redirect_to @user
+		else
+			flash.now[:warning] =	 "Invalid informations"
+			render 'edit'
+		end
+	end
 
 	def destroy
-    @user.destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_path
-  end
+		@user.destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_path
+	end
 
-private
-  
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_path, notice: "Please sign in" 
-    end
-  end
+	private
+	
+	def signed_in_user
+		unless signed_in?
+			store_location
+			redirect_to signin_path, notice: "Please sign in" 
+		end
+	end
 
-  def correct_user
-    @user = User.find params[:id]
-    redirect_to root_path unless current_user? @user
-  end
+	def correct_user
+		@user = User.find params[:id]
+		redirect_to root_path unless current_user? @user
+	end
 
-  def admin_user
-    @user = User.find params[:id]
-    redirect_to root_path unless current_user.admin? && !(current_user? @user)
-  end
+	def admin_user
+		@user = User.find params[:id]
+		redirect_to root_path unless current_user.admin? && !(current_user? @user)
+	end
 
-  def registered_user
-    redirect_to root_path, notice: "You already have an account" if signed_in?
-  end
+	def registered_user
+		redirect_to root_path, notice: "You already have an account" if signed_in?
+	end
 end
