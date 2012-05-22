255d2bcb3d0895f50e9ac6a64274e1c074d53de3
Pawel Batko <pawel.batko@gmail.com>
2012-05-22 21:43:14 +0200

    validate Project model and tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,4 +6,8 @@ class Project < ActiveRecord::Base
 	belongs_to :company
 	
 	attr_accessible :name, :ptype, :location
+	validates :company_id, presence: true
+	validates :location, presence: true, length: {maximum: 128 }
+	validates :name, presence: true, length: {maximum: 32 }
+	validates :ptype, presence: true, inclusion: { in: 1..2 }
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -17,6 +17,50 @@ describe Project do
 	its(:company) {should == company }
 	it { should be_valid }
 
+
+	
+	describe "when name is not blank" do 
+		before { @project.name = "" }
+		it {should_not be_valid}
+	end	
+
+	describe "when location is blank" do 
+		before { @project.location = "" }
+		it {should_not be_valid}
+	end
+
+	describe "when name is too long" do
+		before { @project.name = "a"*33 }
+		it { should_not be_valid }
+	end
+
+
+	describe "when ptype has to high value" do
+		before { @project.location = 2 }
+		it { should_not be_valid }
+	end
+
+
+	describe "when name is not present" do 
+		before { @project.name = nil }
+		it {should_not be_valid}
+	end	
+
+	describe "when ptype is not present" do 
+		before { @project.ptype = nil }
+		it {should_not be_valid}
+	end
+
+	describe "when location is not present" do 
+		before { @project.location = nil }
+		it {should_not be_valid}
+	end
+
+	describe "when company_id is not present" do
+		before { @project.company_id = nil }
+		it { should_not be_valid }
+	end
+
 	describe "accessible attributes" do
 		it "should not allow access to company_id" do
 			expect do
