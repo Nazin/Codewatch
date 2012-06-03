ad9586ba483096e56c6f1f2045b2495e94941a36
Pawel Batko <pawel.batko@gmail.com>
2012-06-04 00:45:54 +0200

    fix

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -3,7 +3,7 @@ module ProjectHelper
 		if project.repository_created
 			"Your repository address: git@codewatch.pl:#{project.location}.git"
 		else
-			link_to "Create git repository", create_project_repo_path(project)
+			link_to "Try again: Create git repository", create_project_repo_path(project), class: 'button r'
 		end
 	end
 
