2700156c4ccdec15aa14344386e0ea13e70e4236
Pawel Batko <pawel.batko@gmail.com>
2012-06-05 00:27:25 +0200

    fix

--- app/controllers/source_controller.rb
+++ app/controllers/source_controller.rb
@@ -34,7 +34,8 @@ class SourceController < ApplicationController
 		
 		if params[:parent]
 			@tree = repo.tree params[:parent]
-			@blob = @tree.blobs.find { |b| b.id == params[:blob_id] }
+
+			@blob = @tree.blobs.select { |b| b.id == params[:blob_id] }
 		else
 			@blob =	repo.blob params[:blob_id]
 		end
@@ -60,6 +61,7 @@ private
 	def root_tree
 		repo = @project.repo
 		tree = repo.commits.first.tree
+		@parent = tree
 		process_tree @tree
 #		tree.id # => "3536eb9abac69c3e4db583ad38f3d30f8db4771f"
 	end 
@@ -67,6 +69,7 @@ private
 	def sub_tree
 		repo = @project.repo
 		tree =	repo.tree params[:tree_id]
+		@parent = tree
 		process_tree tree
 # => #<Grit::Tree "91169e1f5fa4de2eaea3f176461f5dc784796769">
 	end
