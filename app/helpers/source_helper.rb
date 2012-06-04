542c08113801d714a838b3f0930552757a667f45
Pawel Batko <pawel.batko@gmail.com>
2012-06-05 00:33:24 +0200

    fix

--- app/helpers/source_helper.rb
+++ app/helpers/source_helper.rb
@@ -3,11 +3,11 @@ module SourceHelper
 		link_to el.name, project_tree_path(@project, el.id) 
 	end
 
-	def blob el, parent=nil
-		if parent.nil?
+	def blob el, parent_id=nil
+		if parent_id.nil?
 			link_to el.name, project_blob_path(@project, el.id)
 		else
-			link_to el.name, project_parent_blob_path(@project, parent.id, el.id)
+			link_to el.name, project_parent_blob_path(@project, parent_id, el.id)
 		end
 	end
 
