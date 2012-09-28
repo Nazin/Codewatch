262964b141e7f6376d696aac42170e114cc3da09
Piotr Niziniecki <pniziniecki@gmail.com>
2012-09-28 16:35:25 +0200

    little update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -66,10 +66,10 @@ private
 		path = @comment.path
 		
 		if path.nil? || path == ''
-			url = project_blob_path @project, @comment.blob
+			url = project_branch_blob_path @project, @branch, @comment.blob
 		else
 			path.gsub! '/','_'
-			url = project_parent_blob_path @project, path, @comment.blob
+			url = project_parent_branch_blob_path @project, @branch, path, @comment.blob
 		end
 		
 		url += '#comment_' + @comment.id.to_s
